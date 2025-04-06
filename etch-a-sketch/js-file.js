9e209f4266c6319bf8e7aa5d4c46f5593f9518d0
oraziorillo <oraziorillo@gmail.com>
2025-04-06 12:25:53 +0200

    created a small example of DOM manipulation

---
+++ etch-a-sketch/js-file.js
@@ -0,0 +1,24 @@
+const container = document.querySelector("#container");
+
+const p = document.createElement("p");
+p.style.color = "red";
+p.textContent = "Hey I'm red!";
+container.appendChild(p);
+
+const h3 = document.createElement("h3");
+h3.style.color = "blue";
+h3.textContent = "I'm a blue h3!";
+container.appendChild(h3);
+
+const div = document.createElement("div");
+// div.style.border = "black solid 4px";
+
+const h1InDiv = document.createElement("h1");
+h1InDiv.textContent = "I'm in a div";
+div.appendChild(h1InDiv);
+
+const pInDiv = document.createElement("p");
+pInDiv.textContent = "ME TOO!";
+div.appendChild(pInDiv);
+
+container.appendChild(div);
