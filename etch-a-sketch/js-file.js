59b6fa530c7d6bf40bdd539f5a7cba1711075e5c
oraziorillo <oraziorillo@gmail.com>
2025-04-07 12:26:53 +0200

    added button with event listener

--- etch-a-sketch/js-file.js
+++ etch-a-sketch/js-file.js
@@ -25,3 +25,8 @@ container.appendChild(div);
 
 const btn = document.querySelector("#btn");
 btn.onclick = () => alert("Hello World");
+
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", () => {
+    alert("Hello World");
+});
